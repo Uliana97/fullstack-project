441a626ed5112f8fd7f920353bb2a39fc8aadaf8
Uliana <ulidmitrieva97@gmail.com>
2022-02-12 19:27:01 +0100

    set mikro0orm with graphql

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
 
+@ObjectType() //typing
 @Entity()
 export class Post {
+  @Field(() => Int) //typing
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String) //typing
   @Property({ type: "date" })
   createdAt = new Date();
 
+  @Field(() => String) //typing
   @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt = new Date();
 
+  @Field() //typing
   @Property({ type: "text" })
   title!: string;
 }
